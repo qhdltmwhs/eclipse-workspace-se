4eee52c268318b1162ee94347f22288245aa21a7
qhdlt <qhdlt@DESKTOP-1LBBODB>
2020-12-23 03:51:45 +0900

    l

--- 99.REPORT/src/com/itwill04/array/LottoMain.java
+++ 99.REPORT/src/com/itwill04/array/LottoMain.java
@@ -9,7 +9,21 @@ public class LottoMain {
 		
 		for (int i = 0; i < lottoNumber.length; i++) {
 			lottoNumber[i] = (int)(Math.random()*45)+1;
+			for (int j = 0; j < i; j++) {
+				if(lottoNumber[i] == lottoNumber[j]) {
+					System.out.println("중복허용안됨");
+					System.out.println("[i] " + i + " index값 " + lottoNumber[i]);  
+					System.out.println("[j] " + j + " index값 " + lottoNumber[j]);
+					i--;
+					break;
+				}
+			}	
+		}		
+		/*
+		for (int i = 0; i < lottoNumber.length; i++) {
+			lottoNumber[i] = (int)(Math.random()*45)+1;
 		}
+		*/
 		for (int i = 0; i < lottoNumber.length; i++) {
 			System.out.print(lottoNumber[i]+" ");
 		}
