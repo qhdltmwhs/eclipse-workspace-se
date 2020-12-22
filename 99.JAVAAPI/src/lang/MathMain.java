e1b6c36adef7757ce470ec99a347852e35803143
stu24 <stu24@DESKTOP-9E850EU>
2020-12-22 18:48:04 +0900

    d

--- 99.JAVAAPI/src/lang/MathMain.java
+++ 99.JAVAAPI/src/lang/MathMain.java
@@ -3,8 +3,43 @@ package lang;
 public class MathMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/*
+		Math 클래스
+		   - 수학함수를 가지고있는클래스
+		   - 모든 메쏘드 static 이다.
+		   - 객체생성이 불가능
+		      (생성자 접근제한자 private)
+		The constructor Math() is not visible
+		Math math=new Math();
+		 */
+		int r=Math.abs(-89789);
+		System.out.println(r);
+		System.out.println(Math.max(23, 45));
+		System.out.println(Math.min(23, 45));
+		System.out.println(Math.round(33.55555));
+		System.out.println(Math.round(33.55555*10)/10.0);
+		System.out.println(Math.pow(2, 10));
+		System.out.println(Math.sqrt(2));
+		
+		/*
+		
+		 public static double random()
+		      Returns a double value with a positive sign, 
+		       greater than or equal to 0.0 and less than 1.0.
+		 */
+		System.out.println(Math.random());
+		System.out.println("1~10사이의 정수랜덤생성");
+		System.out.println((int)(Math.random()*10)+1);
+		System.out.println((int)(Math.random()*10)+1);
+		System.out.println((int)(Math.random()*10)+1);
+		System.out.println((int)(Math.random()*10)+1);
+		System.out.println("1~45사이의 정수랜덤생성");
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
+		System.out.println((int)(Math.random()*45)+1);
 	}
-
 }
