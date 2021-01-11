6ee3032fe2b7167ce4aa910ae4470f81e7f13558
stu24 <stu24@DESKTOP-9E850EU>
2021-01-11 16:59:20 +0900

    d

---
+++ 21.입출력[IO]/src/byte_stream/filter_stream/AccountFileReadMain.java
@@ -0,0 +1,34 @@
+package byte_stream.filter_stream;
+
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+
+public class AccountFileReadMain {
+
+	public static void main(String[] args) throws Exception{
+		/*
+		 * 파일(accounts.dat) --> Account객체내용읽어서 Account객체생성
+		 */
+		System.out.println(">>>파일(accounts.dat) --> Account객체내용읽어서 Account객체생성");
+		DataInputStream accountDis=
+				new DataInputStream(
+						new FileInputStream("accounts.dat"));
+		Account account1=
+				new Account(accountDis.readInt(),
+						    accountDis.readUTF(),
+						    accountDis.readInt(), 
+						    accountDis.readDouble());
+		
+		Account account2=
+				new Account(accountDis.readInt(),
+						accountDis.readUTF(),
+						accountDis.readInt(), 
+						accountDis.readDouble());
+		accountDis.close();
+		Account.headerPrint();
+		account1.print();
+		account2.print();
+		
+	}
+
+}
