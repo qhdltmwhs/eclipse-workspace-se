26334659537f198a6bedac9d14f8749d241fc435
qhdlt <qhdlt@DESKTOP-1LBBODB>
2021-01-11 22:32:51 +0900

    l

--- 21.입출력[IO]/src/byte_stream/filter_stream/AccountArrayListFileWriteMain.java
+++ 21.입출력[IO]/src/byte_stream/filter_stream/AccountArrayListFileWriteMain.java
@@ -1,10 +1,12 @@
 package byte_stream.filter_stream;
 
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.util.Arrays;
 import java.util.List;
 
-public class AccountArrayListFileWriteMain {
-	public static void main(String[] args) {
+public class AccountArrayListFileWriteMain  {
+	public static void main(String[] args) throws Exception {
 		/*
 		 * List<Account>  --> 파일로저장(accountList.dat)[Quiz]
 		 */
@@ -19,7 +21,18 @@ public class AccountArrayListFileWriteMain {
 				new Account(8888, "GIM", 21000, 2.4),
 				new Account(9999, "QIM", 99000, 3.3));
 		
-
+		
+		DataOutputStream accountListDos =
+				new DataOutputStream(
+						new FileOutputStream("accountList.dat"));
+		accountListDos.writeInt(accountList.size());
+		for (int i = 0; i < accountList.size(); i++) {
+			accountListDos.writeInt(accountList.get(i).getNo());
+			accountListDos.writeUTF(accountList.get(i).getOwner());
+			accountListDos.writeInt(accountList.get(i).getBalance());
+			accountListDos.writeDouble(accountList.get(i).getIyul());
+		}
+		accountListDos.close();
+		System.out.println(">>>>Account객체data --> accounts.dat파일에저장");
 	}
-
 }
